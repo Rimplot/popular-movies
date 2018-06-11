1965280868a1d49df86b86a4d5649bef8bc0e213
Kis Lázár Bence <akislb99@gmail.com>
2018-06-11 14:53:13 +0200

    Properly formatted grid
    • Grid items' look based on grid_item.xml layout
    • Images loaded from URL

--- app/src/main/java/studio/blackhand/popularmovies/ImageAdapter.java
+++ app/src/main/java/studio/blackhand/popularmovies/ImageAdapter.java
@@ -1,6 +1,7 @@
 package studio.blackhand.popularmovies;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -10,13 +11,15 @@ import com.squareup.picasso.Picasso;
 class ImageAdapter extends BaseAdapter {
 
     private Context mContext;
+    private LayoutInflater inflater;
 
     public ImageAdapter(Context c) {
         mContext = c;
+        inflater = LayoutInflater.from(mContext);
     }
 
     public int getCount() {
-        return 10;
+        return images.length;
     }
 
     public Object getItem(int position) {
@@ -29,16 +32,26 @@ class ImageAdapter extends BaseAdapter {
 
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
         if (convertView == null) {
             // no view – create a new one
-            imageView = new ImageView(mContext);
-        } else {
-            // recycle view
-            imageView = (ImageView) convertView;
+            convertView = inflater.inflate(R.layout.grid_item, parent, false);
         }
 
-        Picasso.get().load(R.drawable.pic).into(imageView);
-        return imageView;
+        Picasso
+            .get()
+            .load(images[position])
+            .into((ImageView) convertView);
+
+        return convertView;
     }
+
+    public static String[] images = {
+            "http://image.tmdb.org/t/p/w185/nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg",
+            "http://image.tmdb.org/t/p/w185/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg",
+            "http://image.tmdb.org/t/p/w185/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg",
+            "http://image.tmdb.org/t/p/w185/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
+            "http://image.tmdb.org/t/p/w185/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg",
+            "http://image.tmdb.org/t/p/w185/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+            "http://image.tmdb.org/t/p/w185/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+    };
 }
